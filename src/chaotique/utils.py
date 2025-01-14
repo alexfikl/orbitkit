e7eb42bbfd4984df4d1842da004f951aac0678f8
Alexandru Fikl <alexfikl@gmail.com>
2025-01-14 17:06:09 +0200

    feat: add some scaffolding

---
+++ src/chaotique/utils.py
@@ -0,0 +1,217 @@
+# SPDX-FileCopyrightText: 2025 Alexandru Fikl <alexfikl@gmail.com>
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import logging
+import os
+from typing import Any
+
+# {{{ logging
+
+
+def module_logger(
+    module: str,
+    level: int | str | None = None,
+) -> logging.Logger:
+    """Create a new logging for the module *module*.
+
+    The logger is created using a :class:`rich.logging.RichHandler` for fancy
+    highlighting. The ``NO_COLOR`` environment variable can be used to
+    disable colors.
+
+    :arg module: a name for the module to create a logger for.
+    :arg level: if *None*, the default value is taken to from the
+        ``CHAOTIQUE_LOGGING_LEVEL`` environment variable and falls back to the
+        ``INFO`` level if it does not exist (see :mod:`logging`).
+    """
+    if level is None:
+        level = os.environ.get("CHAOTIQUE_LOGGING_LEVEL", "INFO").upper()
+
+    if isinstance(level, str):
+        level = getattr(logging, level.upper())
+
+    assert isinstance(level, int)
+
+    name, *rest = module.split(".", maxsplit=1)
+    root = logging.getLogger(name)
+
+    if not root.handlers:
+        from rich.highlighter import NullHighlighter
+        from rich.logging import RichHandler
+
+        no_color = "NO_COLOR" in os.environ
+        handler = RichHandler(
+            level,
+            show_time=True,
+            omit_repeated_times=False,
+            show_level=True,
+            show_path=True,
+            highlighter=NullHighlighter() if no_color else None,
+            markup=True,
+        )
+
+        root.addHandler(handler)
+
+    root.setLevel(level)
+    return root.getChild(rest[0]) if rest else root
+
+
+log = module_logger(__name__)
+
+# }}}
+
+
+# {{{ matplotlib helpers
+
+# fmt: off
+BOOLEAN_STATES = {
+    1: True, "1": True, "yes": True, "true": True, "on": True, "y": True,
+    0: False, "0": False, "no": False, "false": False, "off": False, "n": False,
+}
+# fmt: on
+
+
+def check_usetex(*, s: bool) -> bool:
+    try:
+        import matplotlib
+    except ImportError:
+        return False
+
+    try:
+        return bool(matplotlib.checkdep_usetex(s))  # type: ignore[attr-defined]
+    except AttributeError:
+        # NOTE: simplified version from matplotlib
+        # https://github.com/matplotlib/matplotlib/blob/ec85e725b4b117d2729c9c4f720f31cf8739211f/lib/matplotlib/__init__.py#L439=L456
+
+        import shutil
+
+        if not shutil.which("tex"):
+            return False
+
+        if not shutil.which("dvipng"):
+            return False
+
+        if not shutil.which("gs"):  # noqa: SIM103
+            return False
+
+        return True
+
+
+def set_recommended_matplotlib(
+    *,
+    use_tex: bool | None = None,
+    dark: bool | None = None,
+    savefig_format: str | None = None,
+    overrides: dict[str, Any] | None = None,
+) -> None:
+    """Set custom :mod:`matplotlib` parameters.
+
+    These are mainly used in the tests and examples to provide a uniform style
+    to the results using `SciencePlots <https://github.com/garrettj403/SciencePlots>`__.
+    For other applications, it is recommended to use local settings (e.g. in
+    `matplotlibrc`).
+
+    :arg use_tex: if *True*, LaTeX labels are enabled. By default, this checks
+        if LaTeX is available on the system and only enables it if possible.
+    :arg dark: if *True*, a dark default theme is selected instead of the
+        default light one. If *None*, this takes its values from the ``CHAOTIQUE_DARK``
+        boolean environment variable.
+    :arg savefig_format: the format used when saving figures. By default, this
+        uses the ``CHAOTIQUE_SAVEFIG`` environment variable and falls back to
+        the :mod:`matplotlib` parameter ``savefig.format``.
+    :arg overrides: a mapping of parameters to override the defaults. These
+        can also be set separately after this function was called using ``rcParams``.
+    """
+    try:
+        import matplotlib.pyplot as mp
+    except ImportError:
+        return
+
+    # start off by resetting the defaults
+    import matplotlib as mpl
+
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
+    import os
+
+    if use_tex is None:
+        use_tex = "GITHUB_REPOSITORY" not in os.environ and check_usetex(s=True)
+
+    if not use_tex:
+        log.warning("'use_tex' is disabled on this system.")
+
+    if dark is None:
+        tmp = os.environ.get("CHAOTIQUE_DARK", "off").lower()
+        dark = BOOLEAN_STATES.get(tmp, False)
+
+    if savefig_format is None:
+        savefig_format = os.environ.get(
+            "CHAOTIQUE_SAVEFIG", mp.rcParams["savefig.format"]
+        ).lower()
+
+    from contextlib import suppress
+
+    # NOTE: preserve existing colors (the ones in "science" are ugly)
+    prop_cycle = mp.rcParams["axes.prop_cycle"]
+    with suppress(ImportError):
+        import scienceplots  # noqa: F401
+
+        mp.style.use(["science", "ieee"])
+
+    # NOTE: the 'petroff10' style is available for version >= 3.10.0 and changes
+    # the 'prop_cycle' to the 10 colors that are more accessible
+    if "petroff10" in mp.style.available:
+        mp.style.use("petroff10")
+        prop_cycle = mp.rcParams["axes.prop_cycle"]
+
+    defaults: dict[str, dict[str, Any]] = {
+        "figure": {
+            "figsize": (8, 8),
+            "dpi": 300,
+            "constrained_layout.use": True,
+        },
+        "savefig": {"format": savefig_format},
+        "text": {"usetex": use_tex},
+        "legend": {"fontsize": 20},
+        "lines": {"linewidth": 2, "markersize": 10},
+        "axes": {
+            "labelsize": 28,
+            "titlesize": 28,
+            "grid": True,
+            "grid.axis": "both",
+            "grid.which": "both",
+            "prop_cycle": prop_cycle,
+        },
+        "xtick": {"labelsize": 20, "direction": "out"},
+        "ytick": {"labelsize": 20, "direction": "out"},
+        "xtick.major": {"size": 6.5, "width": 1.5},
+        "ytick.major": {"size": 6.5, "width": 1.5},
+        "xtick.minor": {"size": 4.0},
+        "ytick.minor": {"size": 4.0},
+    }
+
+    if dark:
+        # NOTE: this is the black color used by the sphinx-book theme
+        black = "111111"
+        gray = "28313D"
+        defaults["text"].update({"color": "white"})
+        defaults["axes"].update({
+            "labelcolor": "white",
+            "facecolor": gray,
+            "edgecolor": "white",
+        })
+        defaults["xtick"].update({"color": "white"})
+        defaults["ytick"].update({"color": "white"})
+        defaults["figure"].update({"facecolor": black, "edgecolor": black})
+        defaults["savefig"].update({"facecolor": black, "edgecolor": black})
+
+    for group, params in defaults.items():
+        mp.rc(group, **params)
+
+    if overrides:
+        for group, params in overrides.items():
+            mp.rc(group, **params)
+
+
+# }}}
