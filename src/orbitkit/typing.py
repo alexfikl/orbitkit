b58bc0ff4c952f68660768bde2f6ad7663000f81
Alexandru Fikl <alexfikl@gmail.com>
2025-08-06 10:43:35 +0300

    feat: mark type aliases explicitly

--- src/orbitkit/typing.py
+++ src/orbitkit/typing.py
@@ -4,17 +4,21 @@
 from __future__ import annotations
 
 import os
-from typing import Any, TypeVar
+from typing import Any, TypeAlias, TypeVar
 
 import numpy as np
 
 T = TypeVar("T")
 """An unbound invariant generic type variable."""
 
-PathLike = os.PathLike[str] | str
+PathLike: TypeAlias = os.PathLike[str] | str
 """A union of types supported as paths."""
 
-Array = np.ndarray[tuple[int, ...], np.dtype[Any]]
+Array: TypeAlias = np.ndarray[tuple[int, ...], np.dtype[Any]]
 """Array type alias for :class:`numpy.ndarray`."""
-Scalar = np.number[Any] | np.ndarray[tuple[int], np.dtype[np.generic]]
+Float: TypeAlias = float | np.floating[Any]
+"""Type alias for admissible float types."""
+Scalar: TypeAlias = int | float | np.number[Any]
 """Scalar type alias (generally a value convertible to a :class:`float`)."""
+ScalarLike: TypeAlias = Scalar | np.ndarray[tuple[int], np.dtype[np.generic]]
+"""A scalar-like value, which may include array of shape ``()``."""
