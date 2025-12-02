bd53242d33bd624cd098d6173810f3f00d5e738e
Alexandru Fikl <alexfikl@gmail.com>
2025-12-02 11:18:20 +0200

    feat: add BlockTimer helper

--- src/orbitkit/utils.py
+++ src/orbitkit/utils.py
@@ -9,11 +9,14 @@ import time
 from collections.abc import Callable, Iterable, Iterator
 from contextlib import contextmanager
 from dataclasses import dataclass, field
-from typing import Any, Literal
+from typing import TYPE_CHECKING, Any, Literal
 
 import numpy as np
 
 from orbitkit.typing import Array, PathLike, T
+
+if TYPE_CHECKING:
+    from types import TracebackType
 
 # {{{ environment
 
@@ -88,159 +91,6 @@ def module_logger(
 
 
 log = module_logger(__name__)
-
-# }}}
-
-
-# {{{ matplotlib helpers
-
-
-def check_usetex(*, s: bool) -> bool:
-    try:
-        import matplotlib
-    except ImportError:
-        return False
-
-    try:
-        return bool(matplotlib.checkdep_usetex(s))  # type: ignore[attr-defined]
-    except AttributeError:
-        # NOTE: simplified version from matplotlib
-        # https://github.com/matplotlib/matplotlib/blob/ec85e725b4b117d2729c9c4f720f31cf8739211f/lib/matplotlib/__init__.py#L439=L456
-
-        import shutil
-
-        if not shutil.which("tex"):
-            return False
-
-        if not shutil.which("dvipng"):
-            return False
-
-        if not shutil.which("gs"):  # noqa: SIM103
-            return False
-
-        return True
-
-
-def set_plotting_defaults(
-    *,
-    use_tex: bool | None = None,
-    dark: bool | None = None,
-    savefig_format: str | None = None,
-    overrides: dict[str, Any] | None = None,
-) -> None:
-    """Set custom :mod:`matplotlib` parameters.
-
-    These are mainly used in the tests and examples to provide a uniform style
-    to the results using `SciencePlots <https://github.com/garrettj403/SciencePlots>`__.
-    For other applications, it is recommended to use local settings (e.g. in
-    `matplotlibrc`).
-
-    :arg use_tex: if *True*, LaTeX labels are enabled. By default, this checks
-        if LaTeX is available on the system and only enables it if possible.
-    :arg dark: if *True*, a dark default theme is selected instead of the
-        default light one. If *None*, this takes its values from the ``ORBITKIT_DARK``
-        boolean environment variable.
-    :arg savefig_format: the format used when saving figures. By default, this
-        uses the ``ORBITKIT_SAVEFIG`` environment variable and falls back to
-        the :mod:`matplotlib` parameter ``savefig.format``.
-    :arg overrides: a mapping of parameters to override the defaults. These
-        can also be set separately after this function was called using ``rcParams``.
-    """
-    try:
-        import matplotlib.pyplot as mp
-    except ImportError:
-        return
-
-    # start off by resetting the defaults
-    import matplotlib as mpl
-
-    mpl.rcParams.update(mpl.rcParamsDefault)
-
-    import os
-
-    if use_tex is None:
-        use_tex = "GITHUB_REPOSITORY" not in os.environ and check_usetex(s=True)
-
-    if not use_tex:
-        log.warning("'use_tex' is disabled on this system.")
-
-    if dark is None:
-        tmp = os.environ.get("ORBITKIT_DARK", "off").lower()
-        dark = BOOLEAN_STATES.get(tmp, False)
-
-    if savefig_format is None:
-        savefig_format = os.environ.get(
-            "ORBITKIT_SAVEFIG", mp.rcParams["savefig.format"]
-        ).lower()
-
-    from contextlib import suppress
-
-    # NOTE: preserve existing colors (the ones in "science" are ugly)
-    prop_cycle = mp.rcParams["axes.prop_cycle"]
-    with suppress(ImportError):
-        import scienceplots  # noqa: F401
-
-        mp.style.use(["science", "ieee"])
-
-    # NOTE: the 'petroff10' style is available for version >= 3.10.0 and changes
-    # the 'prop_cycle' to the 10 colors that are more accessible
-    if "petroff10" in mp.style.available:
-        mp.style.use("petroff10")
-        prop_cycle = mp.rcParams["axes.prop_cycle"]
-
-    defaults: dict[str, dict[str, Any]] = {
-        "figure": {
-            "figsize": (8, 8),
-            "dpi": 300,
-            "constrained_layout.use": True,
-        },
-        "savefig": {"format": savefig_format},
-        "text": {"usetex": use_tex},
-        "legend": {
-            "fontsize": 20,
-            "frameon": True,
-            "fancybox": False,
-            "edgecolor": "black",
-        },
-        "lines": {"linewidth": 2, "markersize": 10},
-        "axes": {
-            "labelsize": 28,
-            "titlesize": 28,
-            "grid": True,
-            "grid.axis": "both",
-            "grid.which": "both",
-            "prop_cycle": prop_cycle,
-        },
-        "xtick": {"labelsize": 20, "direction": "in"},
-        "ytick": {"labelsize": 20, "direction": "in"},
-        "xtick.major": {"size": 6.5, "width": 1.5},
-        "ytick.major": {"size": 6.5, "width": 1.5},
-        "xtick.minor": {"size": 4.0},
-        "ytick.minor": {"size": 4.0},
-    }
-
-    if dark:
-        # NOTE: this is the black color used by the sphinx-book theme
-        black = "111111"
-        gray = "28313D"
-        defaults["text"].update({"color": "white"})
-        defaults["axes"].update({
-            "labelcolor": "white",
-            "facecolor": gray,
-            "edgecolor": "white",
-        })
-        defaults["xtick"].update({"color": "white"})
-        defaults["ytick"].update({"color": "white"})
-        defaults["figure"].update({"facecolor": black, "edgecolor": black})
-        defaults["savefig"].update({"facecolor": black, "edgecolor": black})
-
-    for group, params in defaults.items():
-        mp.rc(group, **params)
-
-    if overrides:
-        for group, params in overrides.items():
-            mp.rc(group, **params)
-
 
 # }}}
 
@@ -647,13 +497,16 @@ class TicTocTimer:
 
     .. code:: python
 
-        time = TicTocTimer()
-        time.tic()
+        timer = TicTocTimer()
+        timer.tic()
 
         # ... do some work ...
 
-        elapsed = time.toc()
-        print(time)
+        elapsed = timer.toc()
+        print(timer)
+
+    Note that, unlike MATLAB's function, this class also remembers previous
+    calls and gathers statistics. These can be shown with :meth:`stats`.
     """
 
     t_wall_start: float = field(default=0.0, init=False)
@@ -713,6 +566,70 @@ def tictoc(name: str = "timing") -> Iterator[None]:
 
 # }}}
 
+
+# {{{ BlockTimer
+
+
+@dataclass
+class BlockTimer:
+    """A context manager for timing blocks of code.
+
+    .. code:: python
+
+        with BlockTimer("my-code-block") as bt:
+            # ... do some work ...
+
+        print(bt)
+    """
+
+    name: str = "block"
+    """An identifier used to differentiate the timer."""
+
+    t_wall_start: float = field(init=False)
+    t_wall: float = field(init=False)
+    """Total wall time (set after ``__exit__``), obtained from
+    :func:`time.perf_counter`.
+    """
+
+    t_proc_start: float = field(init=False)
+    t_proc: float = field(init=False)
+    """Total process time (set after ``__exit__``), obtained from
+    :func:`time.process_time`.
+    """
+
+    @property
+    def t_cpu(self) -> float:
+        """Total CPU time, obtained from ``t_proc / t_wall``."""
+        return self.t_proc / self.t_wall
+
+    def __enter__(self) -> BlockTimer:
+        self.t_wall = self.t_proc = 0.0
+        self.t_wall_start = time.perf_counter()
+        self.t_proc_start = time.process_time()
+
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        self.t_wall = time.perf_counter() - self.t_wall_start
+        self.t_proc = time.process_time() - self.t_proc_start
+
+    def __str__(self) -> str:
+        import datetime
+
+        t_wall = datetime.timedelta(seconds=round(self.t_wall))
+        return f"{self.name}: {t_wall} wall, {self.t_cpu:.3f}x cpu"
+
+    def pretty(self) -> str:
+        # NOTE: this matches how MATLAB shows the time from `toc`.
+        return f"[{self.name}] Elapsed time is {self.t_wall:.5f} seconds."
+
+
+# }}}
 
 # {{{ timeit
 
