a0eedaa6bef4eb518bfb6f01a48155c282432e27
Alexandru Fikl <alexfikl@gmail.com>
2026-01-14 12:26:13 +0200

    chore: remove unused symengine mapper

--- src/orbitkit/codegen/jitcode.py
+++ src/orbitkit/codegen/jitcode.py
@@ -9,8 +9,6 @@ from dataclasses import dataclass, replace
 from typing import TYPE_CHECKING, Any, ClassVar, TypeAlias
 
 import numpy as np
-from pymbolic import primitives as prim
-from pymbolic.interop.symengine import PymbolicToSymEngineMapper
 
 import orbitkit.symbolic.primitives as sym
 from orbitkit.codegen import Assignment, Code
@@ -20,62 +18,8 @@ from orbitkit.utils import module_logger
 
 if TYPE_CHECKING:
     import jitcode
-    import symengine as sp
 
 log = module_logger(__name__)
-
-
-# {{{ mapper
-
-SymEngineExpression: TypeAlias = "sp.Basic | np.ndarray[tuple[int, ...], np.dtype[Any]]"
-
-
-class SymEngineMapper(PymbolicToSymEngineMapper):
-    input_map: dict[str, SymEngineExpression]
-
-    def __init__(self, input_map: dict[str, SymEngineExpression]) -> None:
-        super().__init__()
-        self.input_map = input_map
-
-    def map_call(self, expr: prim.Call) -> SymEngineExpression:
-        if isinstance(expr.function, prim.Variable):
-            name = expr.function.name
-            try:
-                func = getattr(self.sym.functions, name)
-            except AttributeError:
-                func = self.sym.Function(name)
-
-            from pytools.obj_array import vectorize_n_args
-
-            return vectorize_n_args(func, *[self.rec(par) for par in expr.parameters])
-        else:
-            raise NotImplementedError(expr)
-
-    def map_matrix_symbol(self, expr: sym.MatrixSymbol) -> SymEngineExpression:
-        if expr.name in self.input_map:
-            result = self.input_map[expr.name]
-            assert expr.shape == result.shape
-        else:
-            result = super().map_variable(expr)
-
-        return result
-
-    def map_contract(self, expr: sym.Contract) -> SymEngineExpression:
-        aggregate = self.rec(expr.aggregate)
-        return np.sum(aggregate, axis=expr.axes)
-
-    def map_reshape(self, expr: sym.Reshape) -> SymEngineExpression:
-        aggregate = self.rec(expr.aggregate)
-        return np.reshape(aggregate, shape=expr.shape)
-
-    def map_dot_product(self, expr: sym.DotProduct) -> SymEngineExpression:
-        left = self.rec(expr.left)  # ty: ignore[invalid-argument-type]
-        right = self.rec(expr.right)  # ty: ignore[invalid-argument-type]
-
-        return np.dot(left, right)
-
-
-# }}}
 
 
 # {{{ target
