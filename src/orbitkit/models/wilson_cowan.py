98e5d05b3cd92cad4817b77be5ad37b17a6f93e8
Alexandru Fikl <alexfikl@gmail.com>
2026-02-04 13:03:10 +0200

    feat: find all roots for wilson-cowan model

--- src/orbitkit/models/wilson_cowan.py
+++ src/orbitkit/models/wilson_cowan.py
@@ -6,6 +6,8 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Literal, TypeAlias
 
+import numpy as np
+
 import orbitkit.symbolic.primitives as sym
 from orbitkit.models import Model
 from orbitkit.models.rate_functions import RateFunction
@@ -166,42 +168,17 @@ Methods: TypeAlias = Literal[
 ]
 
 
-def get_wilson_cowan_fixed_point(
+def _get_wilson_cowan_fixed_point(
     sE: RateFunction,
     sI: RateFunction,
     weights: tuple[float, float, float, float],
     forcing: tuple[float, float],
     *,
+    bracket: tuple[float, float] | None = None,
+    x0: tuple[float, float] | None = None,
     rtol: float = 1.0e-8,
-    method: Methods | None = None,
-) -> tuple[float, float]:
-    r"""Find a synchronized fixed point of the one delay Wilson-Cowan system
-    :class:`WilsonCowan1`.
-
-    To find a synchronized fixed point, we assume that all the weight matrices
-    have equal row sums, given by the *weights* tuple. We also assume that the
-    forcing term is uniform and is given by the *forcing* tuple. Under these
-    assumptions and regardless of the delay, we have that a fixed point of the
-    system must satisfy:
-
-    .. math::
-
-        \begin{aligned}
-            E^\star & = S_E(a E^\star - b I^\star + p), \\
-            I^\star & = S_I(c I^\star - d I^\star + q).
-        \end{aligned}
-
-    This system has at least one real solution in :math:`(0, 1)` (and at most 3
-    such solutions).
-
-    :arg sE: parameters for the sigmoid rate function used in the :math:`E` equation.
-    :arg sI: parameters for the sigmoid rate function used in the :math:`I` equation.
-    :arg weights: a tuple ``(a, b, c, d)`` for the row sums of all the weight matrices.
-    :arg forcing: a tuple ``(p, q)`` for the forcing terms.
-
-    :arg method: one of the methods support by :func:`scipy.optimize.root_scalar`.
-    :returns: a fixed point of the system as a tuple ``(Estar, Istar)``.
-    """
+    method: Methods | None = "brentq",
+) -> tuple[float, float] | None:
     from orbitkit.codegen import lambdify
 
     x = sym.Variable("x")
@@ -210,6 +187,10 @@ def get_wilson_cowan_fixed_point(
 
     a, b, c, d = weights
     p, q = forcing
+
+    x1 = 0.0
+    if x0 is None:
+        x0 = (0.5, 0.5)
 
     # NOTE: We essentially have two equations here
     #
@@ -234,9 +215,9 @@ def get_wilson_cowan_fixed_point(
         result = so.root_scalar(  # ty: ignore[no-matching-overload]
             lambda x: x - sI_func(c * E - d * x + q),
             method=method,
-            fprime=lambda x: 1 + sI_prime(c * E - d * x + q) * d,
+            fprime=lambda x: 1 + d * sI_prime(c * E - d * x + q),
             bracket=(0, 1),
-            x0=(0.5, 0.5),
+            x0=x0[1],
             rtol=rtol,
         )
 
@@ -248,20 +229,112 @@ def get_wilson_cowan_fixed_point(
 
     def root_jac(E: float) -> float:
         I = solve_for_i(E)  # noqa: E741
-        return 1.0 + sE_prime(a * E - b * I + p) * a
+        return 1.0 - a * sE_prime(a * E - b * I + p)
+
+    # NOTE: do not use a bracketing method if it can't work
+    if bracket is not None:
+        fa, fb = root_func(bracket[0]), root_func(bracket[1])
+        if fa * fb >= 0.0:
+            x0 = (bracket[0], x0[1])
+            x1 = bracket[1]
+
+            # NOTE: the secant method seems to work reasonably well when we have
+            # two roots, at least in the unit tests (where `newton` fails)..
+            method = "secant"
+            bracket = None
 
     result = so.root_scalar(  # ty: ignore[no-matching-overload]
         root_func,
         method=method,
         fprime=root_jac,
-        bracket=(0, 1),
-        x0=(0.5, 0.5),
+        bracket=bracket,
+        x0=x0[0],
+        x1=x1,
         rtol=rtol,
+        maxiter=1024,
     )
+
+    if not result.converged:
+        return None
+
     E = result.root
     I = solve_for_i(E)  # noqa: E741
 
     return E, I
+
+
+def get_wilson_cowan_fixed_points(
+    sE: RateFunction,
+    sI: RateFunction,
+    weights: tuple[float, float, float, float],
+    forcing: tuple[float, float],
+    *,
+    npoints: int = 32,
+    rtol: float = 1.0e-8,
+    method: Methods | None = "brentq",
+) -> Array:
+    r"""Find the synchronized fixed points of the one delay Wilson-Cowan system
+    :class:`WilsonCowan1`.
+
+    To find a synchronized fixed point, we assume that all the weight matrices
+    have equal row sums, given by the *weights* tuple. We also assume that the
+    forcing term is uniform and is given by the *forcing* tuple. Under these
+    assumptions and regardless of the delay, we have that a fixed point of the
+    system must satisfy:
+
+    .. math::
+
+        \begin{aligned}
+            E^\star & = S_E(a E^\star - b I^\star + p), \\
+            I^\star & = S_I(c I^\star - d I^\star + q).
+        \end{aligned}
+
+    This system has between between 1 and 3 real solutions in :math:`(0, 1)`.
+    We find these solutions by doing a naive grid search, which is not guaranteed
+    to succeed. If more solutions are suspected, choose a finer grid by increasing
+    *npoints*.
+
+    :arg sE: parameters for the sigmoid rate function used in the :math:`E` equation.
+    :arg sI: parameters for the sigmoid rate function used in the :math:`I` equation.
+    :arg weights: a tuple ``(a, b, c, d)`` for the row sums of all the weight matrices.
+    :arg forcing: a tuple ``(p, q)`` for the forcing terms.
+
+    :arg method: one of the methods support by :func:`scipy.optimize.root_scalar`.
+    :returns: an array of shape ``(n, 2)`` for each of the fixed points.
+    """
+
+    from itertools import product
+
+    E = np.linspace(0.0, 1.0, npoints)
+    I = np.linspace(0.0, 1.0, npoints)  # noqa: E741
+
+    fp = []
+    for i, j in product(range(npoints - 1), range(npoints - 1)):
+        if method in {"bisect", "ridder", "brentq", "brenth", "toms748"}:
+            bracket = (E[i], E[i + 1])
+        else:
+            bracket = None
+
+        result = _get_wilson_cowan_fixed_point(
+            sE,
+            sI,
+            weights,
+            forcing,
+            bracket=bracket,
+            x0=(0.5 * (E[i] + E[i + 1]), 0.5 * (I[j] + I[j + 1])),
+            rtol=rtol,
+            method=method,
+        )
+
+        if result is not None:
+            fp.append(result)
+
+    result = np.array(fp)
+
+    d = abs(round(np.log10(rtol))) - 2
+    _, idx = np.unique(np.round(result, decimals=d), axis=0, return_index=True)
+
+    return result[idx]
 
 
 # }}}
--- tests/test_models.py
+++ tests/test_models.py
@@ -17,12 +17,12 @@ TEST_DIRECTORY = TEST_FILENAME.parent
 log = module_logger(__name__)
 set_plotting_defaults()
 
-# {{{ test_wilson_cowan_fixed_point
+# {{{ test_wilson_cowan_fixed_points
 
 
-def test_wilson_cowan_fixed_point() -> None:
+def test_wilson_cowan_fixed_points() -> None:
     from orbitkit.models.rate_functions import SigmoidRate
-    from orbitkit.models.wilson_cowan import get_wilson_cowan_fixed_point
+    from orbitkit.models.wilson_cowan import get_wilson_cowan_fixed_points
 
     def sigmoid(s: SigmoidRate, x: float) -> float:
         return s.a / (1.0 + np.exp(-(x - s.theta) / s.sigma))  # ty: ignore[no-matching-overload]
@@ -30,34 +30,105 @@ def test_wilson_cowan_fixed_point() -> None:
     rng = np.random.default_rng(seed=42)
     rtol = 1.0e-8
 
-    for _ in range(32):
-        theta = rng.uniform(-5.0, 5.0)
+    for _ in range(8):
         sigma = rng.uniform(0.0, 5.0)
-        sE = SigmoidRate(1.0, theta, sigma)
+        sE = SigmoidRate(1.0, 0.0, sigma)
 
-        theta = rng.uniform(-5.0, 5.0)
         sigma = rng.uniform(0.0, 5.0)
-        sI = SigmoidRate(1.0, theta, sigma)
+        sI = SigmoidRate(1.0, 0.0, sigma)
 
         a, b, c, d = rng.uniform(0, 10.0, size=4)
-        p, q = rng.uniform(-5.0, 5.0, size=2)
+        p, q = 0.0, 0.0
 
-        Estar, Istar = get_wilson_cowan_fixed_point(
-            sE, sI, (a, b, c, d), (p, q), rtol=rtol
-        )
+        result = get_wilson_cowan_fixed_points(sE, sI, (a, b, c, d), (p, q), rtol=rtol)
 
-        rE = abs(Estar - sigmoid(sE, a * Estar - b * Istar + p)) / abs(Estar)
-        rI = abs(Istar - sigmoid(sI, c * Estar - d * Istar + q)) / abs(Istar)
-        log.info("point (%.8e, %.8e) residuals E %.8e I %.8e", Estar, Istar, rE, rI)
+        log.info("Roots: %d", result.shape[0])
+        assert 1 <= result.shape[0] <= 3
 
-        assert 0.0 <= Estar <= 1.0
-        assert 0.0 <= Istar <= 1.0
+        for i in range(result.shape[0]):
+            Estar, Istar = result[i]
+            rE = abs(Estar - sigmoid(sE, a * Estar - b * Istar + p)) / abs(Estar)
+            rI = abs(Istar - sigmoid(sI, c * Estar - d * Istar + q)) / abs(Istar)
+            log.info("point (%.8e, %.8e) residuals E %.8e I %.8e", Estar, Istar, rE, rI)
 
-        assert rE < rtol
-        assert rI < rtol
+            assert 0.0 <= Estar <= 1.0
+            assert 0.0 <= Istar <= 1.0
+
+            assert rE < 10 * rtol
+            assert rI < 10 * rtol
+
+    # {{{ known: 1 solution
+
+    sE = SigmoidRate(1.0, 0.0, 2.0)
+    sI = SigmoidRate(1.0, 0.0, 2.0)
+    a, b, c, d = 6.0, -2.0, 6.0, -2.0
+    p = q = -2.0
+
+    result = get_wilson_cowan_fixed_points(sE, sI, (a, b, c, d), (p, q), rtol=rtol)
+    log.info("Result: %s", result)
+
+    # NOTE: we have a symmetric solution here Estar = Istar
+    assert result.shape[0] == 1, result
+    assert abs(result[0, 0] - result[0, 1]) < rtol
+
+    # NOTE: "exact" solution obtained from Mathematica
+    assert abs(result[0, 0] - 0.9406126803877602) < rtol
+
+    # }}}
+
+    # {{{ known: 2 solutions
+
+    sE = SigmoidRate(1.0, 0.0, 1.0)
+    sI = SigmoidRate(1.0, 0.0, 1.0)
+    a, b, c, d = 5.0, -2.0, 5.0, -2.0
+    # FIXME: this was mostly obtained by a manual bisection method :( Need something
+    # more serious to find the two-solution case
+    p = q = -2.7755113894944901
+
+    result = get_wilson_cowan_fixed_points(
+        sE, sI, (a, b, c, d), (p, q), rtol=rtol, method="brentq"
+    )
+    log.info("Result: %s", result)
+
+    # NOTE: we have a symmetric solution here Estar = Istar
+    assert result.shape[0] == 2, result
+    assert abs(result[0, 0] - result[0, 1]) < rtol
+    assert abs(result[1, 0] - result[1, 1]) < rtol
+
+    # NOTE: "exact" solution obtained from Mathematica
+    assert abs(result[0, 0] - 0.1726731621447889) < 2.0 * rtol
+    assert abs(result[1, 0] - 0.9838836642147963) < rtol
+
+    # }}}
+
+    # {{{ known: 3 solutions
+
+    sE = SigmoidRate(1.0, 0.0, 1.0)
+    sI = SigmoidRate(1.0, 0.0, 1.0)
+    a, b, c, d = 5.0, -2.0, 5.0, -2.0
+    p = q = -3.0
+
+    result = get_wilson_cowan_fixed_points(
+        sE, sI, (a, b, c, d), (p, q), rtol=rtol, method="brentq"
+    )
+    log.info("Result: %s", result)
+
+    # NOTE: we have a symmetric solution here Estar = Istar
+    assert result.shape[0] == 3, result
+    assert abs(result[0, 0] - result[0, 1]) < rtol
+    assert abs(result[1, 0] - result[1, 1]) < rtol
+    assert abs(result[2, 0] - result[2, 1]) < rtol
+
+    # NOTE: "exact" solution obtained from Mathematica
+    assert abs(result[0, 0] - 0.08035781815535596) < rtol
+    assert abs(result[1, 0] - 0.3225798774191962) < rtol
+    assert abs(result[2, 0] - 0.9792620601153998) < rtol
+
+    # }}}
 
 
 # }}}
+
 
 if __name__ == "__main__":
     import sys
