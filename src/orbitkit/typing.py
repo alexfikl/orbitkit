51af90406cab6b562162ccdd5995da4dbaa5fc2b
Alexandru Fikl <alexfikl@gmail.com>
2026-02-24 20:12:53 +0200

    feat: add some array types from optype

--- src/orbitkit/typing.py
+++ src/orbitkit/typing.py
@@ -1,13 +1,15 @@
 # SPDX-FileCopyrightText: 2025 Alexandru Fikl <alexfikl@gmail.com>
-# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: optype authors
+# SPDX-License-Identifier: MIT AND BSD-3-Clause
 
 from __future__ import annotations
 
 import os
 from dataclasses import Field
-from typing import Any, ClassVar, Protocol, TypeAlias, TypeVar
+from typing import Any, ClassVar, Protocol, TypeAlias
 
 import numpy as np
+from typing_extensions import TypeAliasType, TypeVar
 
 T = TypeVar("T")
 """An unbound invariant generic type variable."""
@@ -15,18 +17,60 @@ T = TypeVar("T")
 PathLike: TypeAlias = os.PathLike[str] | str
 """A union of types supported as paths."""
 
+# TODO: this is deprecated, use ArrayND
 Array: TypeAlias = np.ndarray[tuple[int, ...], np.dtype[Any]]
 """Array type alias for :class:`numpy.ndarray`."""
-Array1D: TypeAlias = np.ndarray[tuple[int], np.dtype[Any]]
-"""Array type alias for a 1D :class:`numpy.ndarray`."""
-Array2D: TypeAlias = np.ndarray[tuple[int, int], np.dtype[Any]]
-"""Array type alias for a 2D :class:`numpy.ndarray`."""
+
+# TODO: Should probably just depend on `optype`. We already pull it in through
+# `scipy-stubs`. For now this is a very minimalist copy-paste from there. It
+# contains parts of
+#   optype/numpy/_array.py
+#   optype/numpy/_scalar.py
+
+ShapeT = TypeVar("ShapeT", bound=tuple[int, ...], default=tuple[Any, ...])
+ScalarTypeT = TypeVar("ScalarTypeT", bound=np.generic, default=Any)
+
+ArrayND = TypeAliasType(
+    "ArrayND",
+    np.ndarray[ShapeT, np.dtype[ScalarTypeT]],
+    type_params=(ShapeT, ScalarTypeT),
+)
+"""A type alias for a shape and type generic :class:`numpy.ndarray`."""
+
+Array0D = TypeAliasType(
+    "Array0D",
+    np.ndarray[tuple[()], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 0-dimensional :class:`ArrayND`."""
+
+Array1D = TypeAliasType(
+    "Array0D",
+    np.ndarray[tuple[int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 1-dimensional :class:`ArrayND`."""
+
+Array2D = TypeAliasType(
+    "Array0D",
+    np.ndarray[tuple[int, int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+"""A type alias for a 2-dimensional :class:`ArrayND`."""
+
+Array3D = TypeAliasType(
+    "Array0D",
+    np.ndarray[tuple[int, int, int], np.dtype[ScalarTypeT]],
+    type_params=(ScalarTypeT,),
+)
+
+# TODO: we probably want to also support complex "scalars" down the road
 
 Float: TypeAlias = float | np.floating[Any]
 """Type alias for admissible float types."""
 Scalar: TypeAlias = int | float | np.floating[Any]
 """Scalar type alias (generally a value convertible to a :class:`float`)."""
-ScalarLike: TypeAlias = Scalar | np.ndarray[tuple[()], np.dtype[np.number]]
+ScalarLike: TypeAlias = Scalar | Array0D[np.number[Any]]
 """A scalar-like value, which may include array of shape ``()``."""
 
 
