01813630539fa12dc16ed8b1bdc666bfb40e64f5
Alexandru Fikl <alexfikl@gmail.com>
2026-04-20 10:55:04 +0300

    feat: port hiv Lyapunov example to new setup

--- examples/hiv-culshawruanwebb2003-lyapunov.py
+++ examples/hiv-culshawruanwebb2003-lyapunov.py
@@ -7,31 +7,26 @@ import pathlib
 
 import numpy as np
 
+from orbitkit.codegen.jitcxde import has_jitcdde
 from orbitkit.models import (
     constant_past_initial_conditions,
     transform_distributed_delay_model,
 )
 from orbitkit.models.hiv import CulshawRuanWebb, make_model_from_name
-from orbitkit.symbolic.primitives import DiracDelayKernel
 from orbitkit.utils import module_logger, on_ci
 
 log = module_logger(__name__)
 rng = np.random.default_rng(seed=42)
 
-try:
-    import jitcdde
-except ImportError:
-    log.error("This example requires `jitcdde`.")
+if not has_jitcdde():
+    log.error("This example requires 'jitcdde'.")
     raise SystemExit(0) from None
-
 
 # {{{ right-hand side
 
 figname = "Figure44"
 model = make_model_from_name(f"CulshawRuanWebb2003{figname}")
 assert isinstance(model, CulshawRuanWebb)
-assert isinstance(model.h, DiracDelayKernel)
-assert isinstance(model.h.tau, (int, float))
 
 log.info("Model: %s", type(model))
 log.info("Equations:\n%s", model)
@@ -40,14 +35,13 @@ ext_model = transform_distributed_delay_model(model, 1)
 log.info("Model: %s", type(ext_model))
 log.info("Equations:\n%s", ext_model)
 
-from orbitkit.codegen.jitcdde import JiTCDDELyapunovTarget, make_input_variable
+from orbitkit.codegen.jitcdde import JiTCDDETarget
 
-target = JiTCDDELyapunovTarget(nlyapunov=4)
-source_func = target.lambdify_model(ext_model, model.n)
+target = JiTCDDETarget(nlyapunov=4)
+code = target.generate_model_code(ext_model, model.n)
+integrator = target.compile(code, debug=False)
 
-y = make_input_variable(2)
-source = source_func(jitcdde.t, y)
-log.info("\n%s", source)
+log.info("\n%s", integrator.f)
 
 # }}}
 
@@ -57,20 +51,18 @@ tspan = (0.0, 2500.0)
 y0 = constant_past_initial_conditions(
     ext_model, {"C": np.array([5.0e5]), "I": np.array([500.0])}
 )
-
-dde = target.compile(source, y, max_delay=model.h.tau)
-dde.constant_past(y0, time=tspan[0])
-dde.step_on_discontinuities()
+integrator.set_initial_conditions(y0, tspan[0])
+integrator.step_on_discontinuities()
 
 dt = 0.01
-ts = np.arange(tspan[0] + dde.t, tspan[1], dt)
+ts = np.arange(tspan[0], tspan[1], dt)
 ys = np.empty(y0.shape + ts.shape, dtype=y0.dtype)
 
 lyap_loc = np.empty((target.nlyapunov, *ts.shape), dtype=y0.dtype)
 weights = np.empty(ts.shape, dtype=y0.dtype)
 
 for i in range(ts.size):
-    ys[:, i], lyap_loc[:, i], weights[i] = dde.integrate(ts[i])
+    ys[:, i], lyap_loc[:, i], weights[i] = integrator.integrate(ts[i])
 
 # compute global Lyapunov exponent
 weights = weights.reshape(1, -1)
